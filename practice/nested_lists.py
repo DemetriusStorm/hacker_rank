c05d6389429b36fdbe5b250a0a09e226bbb7c4f2
Dmitriy Bulygin <demetrius.storm@gmail.com>
2021-03-22 18:35:14 +0300

    Python practice: dict and if the else statement

--- practice/nested_lists.py
+++ practice/nested_lists.py
@@ -3,19 +3,49 @@ Given the names and grades for each student in a class of N students, store them
 of any student(s) having the second lowest grade.
 Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name
 on a new line.
+Harry
+37.21
+Berry
+37.21
+Tina
+37.2
+Akriti
+41
+Harsh
+39
 """
 
 if __name__ == '__main__':
-    result = []
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        result.append([name, score])
-        print(result)
-
+    journal_scores = {'Harry': 37.21,
+                      'Berry': 37.21,
+                      'Tina': 37.2,
+                      'Akriti': 41,
+                      'Harsh': 39,
+                      'Trash': 37.2,
+                      'Clash': 37.2,
+                      'Slash': 37.21,
+                      'Clisnash': 37.21,
+                      'Smash': 37.21,
+                      }
+    sorted_journal = {k: v for k, v in sorted(journal_scores.items(), key=lambda x: x[1], reverse=True)}
+    list_of_names = []
     counter = 0
-    for scores in sorted(result):
-        name, score = scores
-        print(name, score)
-        counter = score
+    minimum = min(sorted_journal.values())
 
+    while sorted_journal:
+        name, score = sorted_journal.popitem()
+        if score == minimum:
+            if counter == 0:
+                continue
+            elif counter == 1:
+                list_of_names.append(name)
+                minimum = score
+        elif score > minimum:
+            if counter == 2:
+                break
+            elif counter == 0:
+                list_of_names.append(name)
+                minimum = score
+                counter += 1
+
+    print('\n'.join(sorted(list_of_names)))
