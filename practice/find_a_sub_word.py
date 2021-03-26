f742ae277fed4ed6225f8551fbf08ac6b6c60291
Dmitriy Bulygin <demetrius.storm@gmail.com>
2021-03-26 11:56:37 +0300

    Refactoring variable names

--- practice/find_a_sub_word.py
+++ practice/find_a_sub_word.py
@@ -5,8 +5,9 @@ Find A Sub-Word.
 import re
 
 if __name__ == '__main__':
-    num_of_sentence = int(input())
-    sequence = '\n'.join(input() for _ in range(num_of_sentence))
-    num_of_subs = int(input())
-    for _ in range(num_of_subs):
-        print(len(re.findall(r'(?<=\w){0}(?=\w)'.format(input().strip()), sequence)))
+    count_of_sentence = int(input())
+    sequence = '\n'.join(input() for _ in range(count_of_sentence))
+    count_of_subs = int(input())
+    pattern = r'(?<=\w){0}(?=\w)'
+    for _ in range(count_of_subs):
+        print(len(re.findall(pattern.format(input().strip()), sequence)))
