673c08d0557eee9e4938c9d1c0acc7383b90a9ff
Dmitriy Bulygin <demetrius.storm@gmail.com>
2021-04-13 10:31:16 +0300

    practice: find ip4/ip6 or other

--- practice/ip_address_validation.py
+++ practice/ip_address_validation.py
@@ -10,12 +10,28 @@ import re
 
 
 def is_ipv4(sequence):
-    return re.search(r'^[^0](\d{1,3}\.){3}\d{1,3}', sequence)
+    return re.search(r'^((1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])$', sequence)
 
 
 def is_ipv6(sequence):
-    return re.search(r'^([\da-f]{1,4}:){7}([\da-f]{1,4})', sequence)
+    return re.search(r'^([\da-f]{1,4}:){7}[\da-f]{1,4}$', sequence)
 
+
+# # second variant without regex expression
+# for x in range(1, int(input("How many tests? ")) + 1):
+#     string = input("Check: ")
+#     try:
+#         str_ipv4 = string.split(".")
+#         str_ipv6 = string.split(":")
+#         if len(str_ipv4) == 4 and all(0 <= int(n) <= 255 for n in str_ipv4):
+#             print("IPv4")
+#         elif len(str_ipv6) == 8 and all(0 <= int(n, 16) <= 65535 for n in str_ipv6):
+#             print("IPv6")
+#         else:
+#             print("Neither")
+#
+#     except ValueError:
+#         print("Neither")
 
 if __name__ == '__main__':
     count_of_seq = int(input())
